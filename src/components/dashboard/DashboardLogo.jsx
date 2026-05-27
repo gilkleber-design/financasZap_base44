c6557d0005ec69e7f9b5cf115b590c0ec8f5a9d5
Gil Kléber <gilkleber@gmail.com>
2026-05-27 01:11:07 +0000

    Visual edits

--- src/components/dashboard/DashboardLogo.jsx
+++ src/components/dashboard/DashboardLogo.jsx
@@ -4,7 +4,7 @@ export default function DashboardLogo({ className = 'h-7 w-7', variant = 'dark' 
   'https://media.base44.com/images/public/69e8f3a2e9ed0f3c08b392f8/bf28f0603_LogoSVG.svg';
 
   return (
-    <img src="https://media.base44.com/images/public/69e8f3a2e9ed0f3c08b392f8/b13ee8e1b_LogoSVG_fundootransparente.svg"
+    <img src="https://media.base44.com/images/public/69e8f3a2e9ed0f3c08b392f8/bf28f0603_LogoSVG.svg"
 
     alt="FinançasZap"
     className={className} />);
